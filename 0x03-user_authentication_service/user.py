9eed5af3606df7fa8f1eea08bbdf7051cd112812
MalekAjil <aboanasajil@gmail.com>
2024-11-22 18:36:28 -0800

    task0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -19,6 +19,7 @@ class User(Base):
     reset_token = Column(String)
 
     def __repr__(self):
+        """user representation method"""
         return ("<User(id='%d', email='%s', hashed_password='%s', " +
                 "session_id='%s', reset_token='%s')>"
                 % (self.id, self.email, self.hashed_password,
